589db2a22de69d09e12b2a451a063d8047dd983f
tlkh40 <tlkh40@users.noreply.github.com>
2024-06-02 23:26:26 +0800

    remove useless import

--- Program.fs
+++ Program.fs
@@ -1,24 +1,13 @@
-namespace warp.Desktop
+﻿namespace warp.Desktop
 
 open System
 open Avalonia
-open Fabulous.Avalonia
 open warp
 
 
 module Program =
     [<CompiledName "BuildAvaloniaApp">]
     let buildAvaloniaApp () =
-        // AppBuilder`
-        //     // .Configure(fun () ->
-        //     //     let app = Program.startApplication App.program
-        //     //     app.Styles.Add(App.theme)
-        //     //     app.RequestedThemeVariant <- Styling.ThemeVariant.Dark
-        //     //     app.Resources["ToggleSwitchPostContentMargin"] <- 0
-        //     //     app.Resources["ToggleSwitchPreContentMargin"] <- 0
-        //     //     app)
-        //     .LogToTrace(areas = Array.empty)
-        //     .UsePlatformDetect()
         App
             .create()
             .UsePlatformDetect()
